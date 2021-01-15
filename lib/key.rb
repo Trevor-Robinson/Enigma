8aeb349fc0c7d88ebbb38f9762e4d18225006466
Trevor-Robinson <trevor.robi1254@gmail.com>
2021-01-15 13:41:05 -0700

    Create key randomization and subkey assignment methods and associated tests

--- lib/key.rb
+++ lib/key.rb
@@ -1,12 +1,12 @@
 class Key
-  attr_reader :random,
+  attr_reader :key,
               :a_key,
               :b_key,
               :c_key,
               :d_key
 
   def initialize
-    @random = 00000
+    @key = "00000"
     @a_key = 00
     @b_key = 00
     @c_key = 00
@@ -14,7 +14,18 @@ class Key
   end
 
   def randomize
-    @random =rand(99999)
+    @key = sprintf '%05i', rand(99999)
   end
 
+  def assign_key(key)
+    @key = key
+  end
+
+  def assign_subkeys
+    key_array = @key.split('')
+    @a_key = [key_array[0], key_array[1]].join.to_i
+    @b_key = [key_array[1], key_array[2]].join.to_i
+    @c_key = [key_array[2], key_array[3]].join.to_i
+    @d_key = [key_array[3], key_array[4]].join.to_i
+  end
 end
--- test/key_test.rb
+++ test/key_test.rb
@@ -7,7 +7,7 @@ class KeyTest < Minitest::Test
   def test_it_exists_and_has_attributes
     key = Key.new
     assert_instance_of Key, key
-    assert_equal 00000, key.random
+    assert_equal "00000", key.key
     assert_equal 00, key.a_key
     assert_equal 00, key.b_key
     assert_equal 00, key.c_key
@@ -17,8 +17,31 @@ class KeyTest < Minitest::Test
   def test_it_can_generate_random_number
     key = Key.new
     key.randomize
-    key.stubs(:random).returns(23574)
-    assert_equal 23574, key.random
+    key.stubs(:key).returns('23574')
+    assert_equal '23574', key.key
+  end
 
+  def test_random_number_is_always_5_digits
+    key = Key.new
+    100.times do |test|
+      key.randomize
+      assert_equal 5, key.key.length
+    end
+  end
+
+  def test_it_can_take_in_a_set_key
+    key = Key.new
+    key.assign_key('23574')
+    assert_equal '23574', key.key
+  end
+
+  def test_it_can_generate_subkeys
+    key = Key.new
+    key.assign_key('23574')
+    key.assign_subkeys
+    assert_equal 23, key.a_key
+    assert_equal 35, key.b_key
+    assert_equal 57, key.c_key
+    assert_equal 74, key.d_key
   end
 end
