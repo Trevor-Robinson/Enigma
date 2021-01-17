42e584429a64630a77b5b4d862db36922492bf1f
Trevor-Robinson <trevor.robi1254@gmail.com>
2021-01-16 18:19:50 -0700

    Alters how shifts take in arguments

--- lib/shifts.rb
+++ lib/shifts.rb
@@ -1,32 +1,17 @@
-require './lib/key'
-require './lib/offset'
 require 'pry'
 
 class Shifts
   attr_reader :shifts
-  def initialize
-    @shift_key = Key.new
-    @shift_offset = Offset.new
+  def initialize(key, offset)
+    @shift_key = key
+    @shift_offset = offset
     @shifts = {A: 0, B: 0, C: 0, D: 0}
   end
 
-  def shifts_with_inputs(key, date)
-    @shift_key.create_with_assigned_key(key)
-    @shift_offset.create_with_assigned_date(date)
+  def shifts_with_inputs
     @shifts[:A] = @shift_key.a_key + @shift_offset.a_offset
     @shifts[:B] = @shift_key.b_key + @shift_offset.b_offset
     @shifts[:C] = @shift_key.c_key + @shift_offset.c_offset
     @shifts[:D] = @shift_key.d_key + @shift_offset.d_offset
   end
-
-  def random_shifts
-    @shift_key.create_randomize
-    @shift_offset.create_with_new_date
-    @shifts[:A] = @shift_key.a_key + @shift_offset.a_offset
-    @shifts[:B] = @shift_key.b_key + @shift_offset.b_offset
-    @shifts[:C] = @shift_key.c_key + @shift_offset.c_offset
-    @shifts[:D] = @shift_key.d_key + @shift_offset.d_offset
-  end
-
-
 end
--- test/shifts_test.rb
+++ test/shifts_test.rb
@@ -1,25 +1,29 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/shifts'
+require './lib/key'
+require './lib/offset'
 require 'mocha/minitest'
 
 class ShiftsTest < Minitest::Test
   def test_it_exists_and_has_attributes
-    shifts = Shifts.new
+    key = Key.new
+    offset = Offset.new
+    key.create_with_assigned_key("02715")
+    offset.create_with_assigned_date("040895")
+    shifts = Shifts.new(key, offset)
     assert_instance_of Shifts, shifts
     assert_equal ({A: 0, B: 0, C: 0, D: 0}), shifts.shifts
   end
 
   def test_it_can_generate_shifts_with_inputs
-    shifts = Shifts.new
-    shifts.shifts_with_inputs("02715", "040895")
+    key = Key.new
+    offset = Offset.new
+    key.create_with_assigned_key("02715")
+    offset.create_with_assigned_date("040895")
+    shifts = Shifts.new(key, offset)
+    shifts.shifts_with_inputs
     assert_equal ({A: 3, B: 27, C: 73, D: 20}), shifts.shifts
-  end
-
-  def test_it_can_generate_random_shifts
-    shifts = Shifts.new
-    shifts.random_shifts
-    assert_equal 4, shifts.shifts.length
   end
 
 end
